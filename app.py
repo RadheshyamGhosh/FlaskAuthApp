d51f53adcc606a4a4560de8a9fe6580d657a964e
RadheshyamGhosh <shambhughosh843@gmail.com>
2026-02-19 17:16:42 +0530

    Final FlaskAuthApp Registration Form

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "secretkey"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+@app.route('/')
+def home():
+    return redirect(url_for('register'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Backend Validation
+        if not name or not email or not password:
+            flash("All fields are required!", "danger")
+            return redirect(url_for('register'))
+
+        if len(password) < 6:
+            flash("Password must be at least 6 characters.", "danger")
+            return redirect(url_for('register'))
+
+        if User.query.filter_by(email=email).first():
+            flash("Email already registered!", "danger")
+            return redirect(url_for('register'))
+
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Registration Successful!", "success")
+        return redirect(url_for('register'))
+
+    return render_template('register.html')
+
+@app.route('/employees')
+def employees():
+    all_users = User.query.all()
+    return render_template('employees.html', users=all_users)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    flash("Employee Deleted Successfully!", "success")
+    return redirect('/employees')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    user = User.query.get_or_404(id)
+
+    if request.method == 'POST':
+        user.name = request.form['name']
+        user.email = request.form['email']
+
+        db.session.commit()
+        flash("Employee Updated Successfully!", "success")
+        return redirect('/employees')
+
+    return render_template('update.html', user=user)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
